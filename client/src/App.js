8a80df02f163caf3cfb6e52d96b191da83dc1aa6
KaitlynBro <kaitlynbrouwers@gmail.com>
2019-05-10 18:23:30 -0400

    volunteer responsive section

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-
+// import Nav from './Nav';
+// import Header from './Header';
+// import Services from './Services';
+// import Causes from './Causes';
+// import Info from './Info';
+// import Donate from './Donate';
+// import Stats from './Stats';
+// import Events from './Events';
+import Volunteer from './Volunteer';
 import Footer from './Footer';
 import './App.css';
 
@@ -7,7 +15,15 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-
+        {/* <Nav />
+        <Header />
+        <Services />
+        <Causes />
+        <Info />
+        <Donate />
+        <Stats />
+        <Events /> */}
+        <Volunteer />
         <Footer />
       </div> 
     )
